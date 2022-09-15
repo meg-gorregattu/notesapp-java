a89598cf0336ac4fe9298db36d03eabf462b1949
Meg Gorregattu <meghanago@gmail.com>
2022-09-15 15:12:59 -0500

    first commit

---
+++ java-proj-4/starter/src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,37 @@
+package com.devmountain.noteApp.entities;
+
+
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+
+    public Note(NoteDto noteDto){
+        if (noteDto.getBody() != null){
+            this.body = noteDto.getBody();
+        }
+    }
+}
